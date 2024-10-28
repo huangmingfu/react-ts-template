a72763a06883889f4b4746ca0bc3a4df7691e938
黄明富 <212149997@qq.com>
2024-10-28 17:40:06 +0800

    feat: update

---
+++ .env.pro
@@ -0,0 +1,14 @@
+# 环境
+VITE_NODE_ENV = production
+
+# 端口号
+VITE_APP_PORT = 5173
+
+# 接口前缀
+VITE_APP_BASE_API = '/pro-api'
+
+# 后端服务地址
+VITE_APP_SERVICE_API = 'http://localhost:8888'
+
+# 输出路径
+VITE_OUT_DIR = dist-pro
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import process from 'node:process'
 
-export default defineConfig(({mode}: ConfigEnv): UserConfig => {
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   // 获取`.env`环境配置文件
   const env = loadEnv(mode, process.cwd())
   return {
@@ -13,9 +13,41 @@ export default defineConfig(({mode}: ConfigEnv): UserConfig => {
         '@': path.resolve(__dirname, 'src')
       }
     },
+    // css: {
+    //   preprocessorOptions: {
+    //     scss: {
+    //       javascriptEnabled: true,
+    //       additionalData: `@use "@/styles/global.scss" as *;` // 引入全局scss变量、方法等
+    //     }
+    //   }
+    // },
+    // 反向代理解决跨域问题
+    server: {
+      // open: true,// 运行时自动打开浏览器
+      host: '0.0.0.0', // 局域网别人也可访问
+      port: Number(env.VITE_APP_PORT), //端口号
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_SERVICE_API,
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '')
+        }
+      }
+    },
+    esbuild:
+      env.VITE_NODE_ENV === 'develop'
+        ? undefined
+        : {
+          /** 打包时移除 console.log */
+          pure: ['console.log'],
+          /** 打包时移除 debugger */
+          drop: ['debugger'],
+          /** 打包时移除所有注释 */
+          legalComments: 'none',
+        },
     build: {
       // target: 'es2015',
-      outDir: 'dist',
+      outDir: env.VITE_OUT_DIR || 'dist',
       minify: 'esbuild',
       chunkSizeWarningLimit: 1500,
       rollupOptions: {
