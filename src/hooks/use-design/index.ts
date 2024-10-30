c7391555b68235ee0f8bd5f8f0207db5da3ce3d4
黄明富 <212149997@qq.com>
2024-10-30 10:55:19 +0800

    feat: update

--- src/hooks/use-design/index.ts
+++ src/hooks/use-design/index.ts
@@ -1,5 +1,10 @@
+import { useMemo } from 'react';
+
 export function useDesign(scope: string) {
-  return {
-    prefixCls: scope // 前缀：'pg'页面、'cmp'组件、'pub'公共组件
-  };
+  return useMemo(
+    () => ({
+      prefixCls: scope // 前缀：'pg'页面、'cmp'组件、'pub'公共组件
+    }),
+    [scope]
+  );
 }
