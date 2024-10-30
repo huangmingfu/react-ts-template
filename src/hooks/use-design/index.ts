9544ed7455e0e352aef939a5840b353c1d123e3c
黄明富 <212149997@qq.com>
2024-10-30 11:10:26 +0800

    feat: update

--- src/hooks/use-design/index.ts
+++ src/hooks/use-design/index.ts
@@ -8,3 +8,13 @@ export function useDesign(scope: string) {
     [scope]
   );
 }
+
+/**
+ * 如果不想在组件内使用的话，可以更名为：（并且去掉useMemo，直接返回对象即可）
+ * getPrefixCls/createDesign
+ *
+ * 因为use开头的hooks会报eslint错误：
+ * React Hook "useDesign" cannot be called at the top level.
+ * React Hooks must be called in a React function component or a custom React Hook function.
+ * eslintreact-hooks/rules-of-hooks
+ */
