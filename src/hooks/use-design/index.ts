2b2c18d09b5a9a25b427e7f668e5994eda13d05a
黄明富 <212149997@qq.com>
2024-10-31 14:15:11 +0800

    feat: update

--- src/hooks/use-design/index.ts
+++ src/hooks/use-design/index.ts
@@ -10,8 +10,7 @@ export function useDesign(scope: string) {
 }
 
 /**
- * 如果不想在函数组件内使用的话，可以更名为：（并且去掉useMemo，直接返回对象即可）
- * getPrefixCls/createDesign
+ * 如果不想在函数组件内使用的话，可以更名为：getPrefixCls/createDesign（并且去掉useMemo，直接返回对象即可）
  *
  * 因为use开头的hooks会报eslint错误：
  * 可以查看官方文档的 hook 规则：Hook 比普通函数更为严格。你只能在你的组件（或其他 Hook）的 顶层 调用 Hook。
