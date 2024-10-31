43f7166f28a95dcb4a5d98142ace65e552ed8fcb
黄明富 <212149997@qq.com>
2024-10-31 10:24:39 +0800

    feat: update

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { useDesign } from '@/hooks';
+import { useDesign, useRouter } from '@/hooks';
 
 import './index.scss';
 
@@ -9,12 +9,16 @@ function Home() {
 
   const [count, setCount] = useState(0);
 
+  const router = useRouter();
+
   return (
     <>
       <div className={prefixCls}>
         <span className={`${prefixCls}__count`}>{count}</span>
       </div>
       <button onClick={() => setCount(count + 1)}>count++</button>
+      <br />
+      <button onClick={() => router.push('/guild/create')}>跳转guild/create页面</button>
     </>
   );
 }
