54b996ad0309cc0d9cd2d17f9bc90cd6af052790
黄明富 <212149997@qq.com>
2024-10-31 11:50:35 +0800

    feat: update

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,16 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 获取提交描述信息
-commitDescInfo=${1:-"feat: update"}  # 默认提交信息
+# 获取提交描述信息，默认为 "feat: update"
+commitDescInfo=${1:-"feat: update"}
 
 git add .
-
-# 判断提交描述信息是否为空
-if [[ -z "${commitDescInfo}" ]]; then
-  git commit -m "feat: update"
-else
-  git commit -m "${commitDescInfo}"
-fi
-
+git commit -m "${commitDescInfo}"
 git push
