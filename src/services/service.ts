98129bf47f1a3b85396d112c62c765633e93e9b5
黄明富 <212149997@qq.com>
2024-11-02 12:04:49 +0800

    perf: 优化service、import类型

--- src/services/service.ts
+++ src/services/service.ts
@@ -24,12 +24,35 @@ axiosInstance.interceptors.request.use(
 // 响应拦截器即异常处理
 axiosInstance.interceptors.response.use(
   (res: AxiosResponse) => {
+    // const code = res.data.code;
+    // switch (code) {
+    //   case 200:
+    //     return res.data;
+    //   case 401:
+    //     /** 登录失效逻辑... */
+    //     return res.data || {};
+    //   default:
+    //     return res.data || {};
+    // }
     return res; //res.data
   },
   (err: AxiosError) => {
+    // 如果接口请求报错时，也返回对象，如return { message: onErrorReason(error.message) }，这样使用async/await就不需要加try/catch
+    // onErrorReason(err.message) // 做一些全局的错误提示，可用ui库的message提示组件
     return Promise.resolve(err);
   }
 );
+
+/** 解析http层面请求异常原因 */
+// function onErrorReason(message: string): string {
+//   if (message.includes('Network Error')) {
+//     return '网络异常，请检查网络情况!';
+//   }
+//   if (message.includes('timeout')) {
+//     return '请求超时，请重试!';
+//   }
+//   return '服务异常,请重试!';
+// }
 
 // 导出实例
 export default axiosInstance;
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,11 @@
+declare module '*.css' {}
+declare module '*.scss' {}
+declare module '*.json' {}
+declare module '*.png' {}
+declare module '*.jpg' {}
+declare module '*.svg' {}
+declare module '*.js' {}
+
 declare interface IResponse<T = unknown> {
   code: number;
   message: string;
