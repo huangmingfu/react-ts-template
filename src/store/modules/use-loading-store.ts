c006ddc7d7486b6ca93d94474e5dc0292d835d22
黄明富 <212149997@qq.com>
2024-11-02 15:54:10 +0800

    docs: 添加注释

--- src/store/modules/use-loading-store.ts
+++ src/store/modules/use-loading-store.ts
@@ -10,6 +10,10 @@ type Action = {
   hideLoading: Fn;
 };
 
+/**
+ * 场景：
+ * 并发请求，响应有快有慢，第一个请求完成后就给loading设置false结束了，实际我们要最后一个请求完成之后才结束loading，所以需要计数。
+ */
 export const useLoadingStore = create<State & Action>((set) => ({
   count: 0, // 用于记录并发请求的数量
   isLoading: false,
